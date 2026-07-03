56125bdb7241394de49bafdefef119943897bfcb
srushtisp09 <srushtipattanshetti99@gmail.com>
2026-07-03 09:07:30 +0530

    Valid parenthesis

---
+++ Stacks/validparenthesis.java
@@ -0,0 +1,29 @@
+package Stacks;
+import java.util.*;
+public class validparenthesis {
+public static boolean isValid(String str){
+    Stack<Character> s=new Stack<>();
+    for(int i=0;i<str.length();i++){
+        char ch=str.charAt(i);
+        if(ch=='('||ch=='{'||ch=='['){
+            s.push(ch);
+        }else{
+            if(s.isEmpty()){
+                return false;
+            }else{
+                if(s.peek()=='(' && ch==')' || s.peek()=='{' && ch=='}' || s.peek()=='[' && ch==']'){
+                    s.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+    }
+    return s.isEmpty();
+}
+public static void main(String args[]){
+String str="({[]})";
+System.out.println(isValid(str));
+} 
+}
+ 
