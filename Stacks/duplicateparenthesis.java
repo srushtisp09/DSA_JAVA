227f557d7b52b653333e5fd0cea1b08ad82575bb
srushtisp09 <srushtipattanshetti99@gmail.com>
2026-07-03 21:35:27 +0530

    Duplicate Parenthesis

---
+++ Stacks/duplicateparenthesis.java
@@ -0,0 +1,29 @@
+package Stacks;
+import java.util.*;
+public class duplicateparenthesis {
+    public static boolean isDuplicate(String str){
+        Stack<Character> s=new Stack<>();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch==')'){
+                int count=0;
+                while(s.pop()!='('){
+                    
+                    count++;
+                }
+                if(count<1){
+                    return true;
+                }else{
+                    s.pop();
+                }
+            }else{
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        String str="((a+b))";
+        System.out.println(isDuplicate(str));
+    }
+}
