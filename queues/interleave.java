94fc3f84d0a8a6f569e650a5e5dcdcb65c5bbd5f
srushtisp09 <srushtipattanshetti99@gmail.com>
2026-07-11 20:39:11 +0530

    Reverse queue

---
+++ queues/interleave.java
@@ -0,0 +1,34 @@
+package queues;
+import java.util.*;
+public class interleave {
+    public static void interleaveQueue(Queue<Integer> q){
+        Queue<Integer> firstHalf=new LinkedList<>();
+        int size=q.size()/2;
+        for(int i=0;i<size;i++){
+             firstHalf.add(q.remove());
+        }
+        while(!firstHalf.isEmpty()){
+            q.add(firstHalf.remove());
+            q.add(q.remove());
+        }
+
+    }
+    public static void main(String args[]){
+Queue<Integer> q=new LinkedList<>();  
+q.add(1);
+q.add(2);
+q.add(3);
+q.add(4);
+q.add(5);
+q.add(6);
+q.add(7);
+q.add(8);
+q.add(9);
+q.add(10);
+interleaveQueue(q);
+while(!q.isEmpty()){
+    System.out.println(q.remove());
+}
+
+}
+}
