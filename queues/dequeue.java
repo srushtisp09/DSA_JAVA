1245c3dbafde920180efba685a015ce4c1a7a0ea
srushtisp09 <srushtipattanshetti99@gmail.com>
2026-07-11 20:46:48 +0530

    Deque

---
+++ queues/dequeue.java
@@ -0,0 +1,13 @@
+package queues;
+import java.util.*;
+
+public class dequeue {
+    public static void main(String args[]){
+        Deque<Integer> deque=new LinkedList<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+        
+        System.out.println(deque );
+    }
+}
