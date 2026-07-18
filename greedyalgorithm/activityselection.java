cd064f42b9687de43a0c3b9a116a6c24f36dcf12
srushtisp09 <srushtipattanshetti99@gmail.com>
2026-07-18 19:59:47 +0530

    Add Greedy algo problem

---
+++ greedyalgorithm/activityselection.java
@@ -0,0 +1,34 @@
+// package greedyalgorithm;
+// import java.util.*;
+// public class activityselection {
+//     public static void main(String args[]){
+        // int start[]={1,3,0,5,8,5};
+        // int end[]={2,4,6,7,9,9};
+        // int maxActivity=0;
+        // ArrayList<Integer> ans=new ArrayList<>();
+        // maxActivity=1;
+        // ans.add(0);
+        // int lastEnd=end[0];
+        // for(int i=1;i<end.length;i++){
+        //     if(start[i]>=lastEnd){
+        //         maxActivity++;
+        //         ans.add(i);
+        //         lastEnd=end[i];
+
+        //     }
+        // }
+        // System.out.println("MAx Acitivity:"+maxActivity);
+        // for(int i=0;i<ans.size();i++){
+        //     System.out.println("A"+ans.get(i)+" ");
+
+
+        // }
+        // System.out.println();
+
+
+
+        ///if the array is not sorted then we need to sort the array based on the 2d array
+  
+//  }
+// }
+
