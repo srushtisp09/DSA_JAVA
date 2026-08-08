b73bf96a831a1d008cfcc2b55a06b7da1a571e38
srushtisp09 <srushtipattanshetti99@gmail.com>
2026-08-08 08:23:28 +0530

    Updated

--- BST/bst.java
+++ BST/bst.java
@@ -11,27 +11,7 @@ public class bst {
             this.right=null;
         }
     }
-    public static Node insert(Node root,int val){
-         if(root==null){
-            root=new Node(val);
-            return root;
-         }
-         if(root.data>val){
-            root.left=insert(root.left,val);
-         }else{
-            root.right=insert(root.right,val);
-         }
-         return root;
-    } 
     
-    public static void inorder(Node root){
-        if(root==null){
-            return ;
-        }
-        inorder(root.left);
-        System.out.println(root.data);
-        inorder(root.right);
-    }
     public static Node findInorderSuccessor(Node root){
         while(root.left!=null){
             root=root.left;
