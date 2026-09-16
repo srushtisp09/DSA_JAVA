54880bb2a8d8d3db888d8a8f208b8084c214ce93
srushtisp09 <srushtipattanshetti99@gmail.com>
2026-09-16 11:40:42 +0530

    Added Connect N ropes with MInimum cost

--- Heaps/ConnectNRopes.java
+++ Heaps/ConnectNRopes.java
@@ -1,5 +1,23 @@
 package Heaps;
-
+//Given are N ropes of different lengths the task is to connnect theses ropes into one rope with minimum cost ,such that the cost to connect two ropes
+// into one rope with minimum cost such that the cost to connect two ropes is equal to the sum of their lengths.
+import java.util.*;
 public class ConnectNRopes {
-    
+ 
+    public static void main(String args[]){
+        int ropes[]={2,3,3,4,6};
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i=0;i<ropes.length;i++){
+            pq.add(ropes[i]);
+
+        }
+        int cost=0;
+        while(pq.size()>1){
+            int min=pq.remove();
+            int min2=pq.remove();
+            cost+=min+min2;
+            pq.add(min+min2);
+        } 
+        System.out.println(cost);
+   }
 }
